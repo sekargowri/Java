9302b0efa3e23371f09a7230403f35a44ed68de1
Gowri <Gowri@192.168.1.3>
2025-08-18 16:32:27 +0530

    created java reference datatypes

---
+++ Java/src/javadatatypes/ReferenceDatatypes.java
@@ -0,0 +1,12 @@
+package javadatatypes;
+
+public class ReferenceDatatypes {
+
+	public static void main(String[] args) {
+
+		String productName = "Wireless Mouse";
+		System.out.println("Product Name: " + productName);
+
+	}
+
+}
