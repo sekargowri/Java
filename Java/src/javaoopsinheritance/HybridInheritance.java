3f487b4ccdb26a73f0de3288178e6901a7d7c03d
Gowri <Gowri@192.168.1.3>
2025-08-24 09:39:30 +0530

    created hybrid inheritance

--- Java/src/javaoopsinheritance/HybridInheritance.java
+++ Java/src/javaoopsinheritance/HybridInheritance.java
@@ -1,5 +1,21 @@
 package javaoopsinheritance;
 
+class User {
+	void login(String username, String password) {
+		System.out.println("User logged in");
+	}
+}
+
+class Customer extends User {
+	void browseProducts() {
+		System.out.println("Customer is browsing products");
+	}
+
+	void placeOrder() {
+		System.out.println("Customer placed order");
+	}
+}
+
 interface LoyaltyProgram {
 	void applyDiscount();
 }
