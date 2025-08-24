46d1ab52330ae53b9733c963904334d492251bf5
Gowri <Gowri@192.168.1.3>
2025-08-24 09:02:35 +0530

    created java inheritance

---
+++ Java/src/javaoopsinheritance/JavaInheritance.md
@@ -0,0 +1,17 @@
+
+# Java Inheritance Types
+
+## Single Inheritance
+```
+One class inherits from a single parent class.
+```
+
+## Multilevel Inheritance
+```
+A class inherits from a class which itself inherits from another class.
+```
+
+## Hierarchical Inheritance
+```
+Multiple classes inherit from a single parent class.
+```
---
+++ Java/src/javaoopsinheritance/SingleInheritance.java
@@ -0,0 +1,28 @@
+package javaoopsinheritance;
+
+class User {
+	void login(String username, String password) {
+		System.out.println("User logged in");
+	}
+}
+
+class Customer extends User {
+	void browseProducts() {
+		System.out.println("Customer is browsing products");
+	}
+}
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+
+		User user = new User();
+		user.login("user1", "user_123");
+
+		Customer customer = new Customer();
+		customer.login("user2", "user123");
+		customer.browseProducts();
+
+	}
+
+}
