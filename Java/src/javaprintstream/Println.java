894723e405f43c6081512150818943fb09e6507b
Gowri <Gowri@192.168.1.5>
2026-01-07 20:51:31 +0530

    created java printstream class println method

---
+++ Java/src/javaprintstream/Println.java
@@ -0,0 +1,13 @@
+package javaprintstream;
+
+public class Println {
+
+	public static void main(String[] args) {
+
+		// println adds a new line after printing
+		// prints + new line
+		System.out.println("Hi");
+		System.out.println("Thank You For Visting My Profile");
+	}
+
+}
