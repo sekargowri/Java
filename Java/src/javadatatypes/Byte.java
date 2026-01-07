fcb5767128473943952300f2d8414dfc4ea4ff27
Gowri <Gowri@192.168.1.5>
2026-01-07 20:55:13 +0530

    created java byte primitive datatype

---
+++ Java/src/javadatatypes/Byte.java
@@ -0,0 +1,14 @@
+package javadatatypes;
+
+public class Byte {
+
+	public static void main(String[] args) {
+
+		byte number = 123; // 3 digit
+
+		byte myDoorNumber = 108;
+		System.out.println("My Door Number is: " + myDoorNumber);
+
+	}
+
+}
