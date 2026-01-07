1f0216759db966c559b3d4cacf35e8b9ee9cf2f6
Gowri <Gowri@192.168.1.5>
2026-01-07 20:51:15 +0530

    created java printstream class print method

---
+++ Java/src/javaprintstream/Print.java
@@ -0,0 +1,14 @@
+package javaprintstream;
+
+public class Print {
+
+	public static void main(String[] args) {
+
+		// print keeps output on the same line
+		// prints + stays on same line
+		System.out.print("I AM Gowri, ");
+		System.out.print("I AM Learning Java");
+
+	}
+
+}
