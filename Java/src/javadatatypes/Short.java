516abc64ae504f3e71a80a91f1bd193dd525bf02
Gowri <Gowri@192.168.1.5>
2026-01-09 10:28:57 +0530

    created java short primitive datatype

---
+++ Java/src/javadatatypes/Short.java
@@ -0,0 +1,14 @@
+package javadatatypes;
+
+public class Short {
+
+	public static void main(String[] args) {
+
+		short number = 12345; // 5 digit
+
+		short myPinCode = 21560;
+		System.out.println("My Pin Code is: " + myPinCode);
+
+	}
+
+}
