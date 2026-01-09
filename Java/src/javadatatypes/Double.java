266f96c40817a481cc67650b355f3974aa31df91
Gowri <Gowri@192.168.1.5>
2026-01-09 10:43:30 +0530

    created java double primitive datatype

---
+++ Java/src/javadatatypes/Double.java
@@ -0,0 +1,14 @@
+package javadatatypes;
+
+public class Double {
+
+	public static void main(String[] args) {
+
+		double decimalNumber = 123456789.123456789;
+
+		double productPrice = 499.00;
+		System.out.println("Product Price: " + productPrice);
+
+	}
+
+}
