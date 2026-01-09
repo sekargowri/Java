ad66276087e1121a48bccb3965d8bf3b16eea085
Gowri <Gowri@192.168.1.5>
2026-01-09 10:34:24 +0530

    created java int primitive datatype

---
+++ Java/src/javadatatypes/Int.java
@@ -0,0 +1,17 @@
+package javadatatypes;
+
+public class Int {
+
+	public static void main(String[] args) {
+
+		int number = 1234567890; // 9 digit
+
+		// int number = 0123456789;
+		// The literal 0123456789 of type int is out of range
+
+		int myPhoneNumber = 987654321;
+		System.out.println("My Phone Number is: " + myPhoneNumber);
+
+	}
+
+}
