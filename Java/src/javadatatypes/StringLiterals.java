d44287f9ff707547ae42fca15f049c357f2dc89a
Gowri <Gowri@192.168.1.5>
2026-01-09 10:58:05 +0530

    created java string literals

---
+++ Java/src/javadatatypes/StringLiterals.java
@@ -0,0 +1,11 @@
+package javadatatypes;
+
+public class StringLiterals {
+
+	public static void main(String[] args) {
+
+		String myName = "Gowri";
+		System.out.println("My Name is: " + myName);
+	}
+
+}
