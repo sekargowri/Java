dba4897aad1169bef29e45ccabfba5b31a676d8a
Gowri <Gowri@192.168.1.5>
2026-01-09 10:36:41 +0530

    created java long primitive datatype

---
+++ Java/src/javadatatypes/Long.java
@@ -0,0 +1,17 @@
+package javadatatypes;
+
+public class Long {
+
+	public static void main(String[] args) {
+
+		long number = 123456789123456789L;
+
+		// long number = 0123456789123456789L;
+		// The literal 0123456789123456789L of type long is out of range
+
+		long myAccountNumber = 987654321123456789L;
+		System.out.println("My Account Number is: " + myAccountNumber);
+
+	}
+
+}
