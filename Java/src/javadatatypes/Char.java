0aa13179bfd2ab6c50eb4e9d16722fcfb7ef36cd
Gowri <Gowri@192.168.1.5>
2026-01-09 10:48:31 +0530

    created java char primitive datatype

---
+++ Java/src/javadatatypes/Char.java
@@ -0,0 +1,22 @@
+package javadatatypes;
+
+public class Char {
+
+	public static void main(String[] args) {
+
+		char character = 'G';
+
+		char number = '4';
+		System.out.println(number); // 4
+
+		// char number = '425';
+		// Invalid character constant
+
+		char numberchar = 04;
+		System.out.println(numberchar); // 
+
+		char currencySymbol = '₹';
+		System.out.println("Currency Symbol is: " + currencySymbol);
+	}
+
+}
