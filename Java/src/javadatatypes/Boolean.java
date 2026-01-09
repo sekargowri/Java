d448b40ced7457728073068792b0f16f52fed5ee
Gowri <Gowri@192.168.1.5>
2026-01-09 10:57:05 +0530

    created java boolean primitive datatype

---
+++ Java/src/javadatatypes/Boolean.java
@@ -0,0 +1,15 @@
+package javadatatypes;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+
+		boolean truevalue = true;
+		boolean falsevalue = false;
+
+		boolean isProductInStock = true;
+		System.out.println("Is Product In Stock: " + isProductInStock);
+
+	}
+
+}
