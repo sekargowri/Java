c53d0ca293feac3201730a384e2b01ca1bee68b2
Gowri <Gowri@192.168.1.5>
2026-01-09 10:41:51 +0530

    created java float primitive datatype

---
+++ Java/src/javadatatypes/Float.java
@@ -0,0 +1,14 @@
+package javadatatypes;
+
+public class Float {
+
+	public static void main(String[] args) {
+
+		float decimalNumber = 12456789.123456789F;
+
+		float productRating = 4.5F;
+		System.out.println("Product Rating is: " + productRating);
+
+	}
+
+}
