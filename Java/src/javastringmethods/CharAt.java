c06d0674716c5584c612bac4628e703fa0e14176
Gowri <Gowri@192.168.1.5>
2026-01-12 20:05:18 +0530

    created java string class charAt() method

--- Java/src/javastringmethods/CharAt.java
+++ Java/src/javastringmethods/CharAt.java
@@ -5,7 +5,13 @@ public class CharAt {
 	public static void main(String[] args) {
 
 		String myFullName = new String("Gowri Sekar");
+
+		// public char charAt(int index);
+
+		System.out.println(myFullName.charAt(0)); // G
+		System.out.println(myFullName.charAt(1)); // o
 		System.out.println(myFullName.charAt(4)); // i
+		System.out.println(myFullName.charAt(5)); //
 
 	}
 
