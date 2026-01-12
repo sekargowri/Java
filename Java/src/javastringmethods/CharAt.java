07a3e0ec1fbae2bd13d74e3a9e15e7ebf4ee414d
Gowri <Gowri@192.168.1.5>
2026-01-12 19:59:25 +0530

    created java string class chartAt() method

---
+++ Java/src/javastringmethods/CharAt.java
@@ -0,0 +1,12 @@
+package javastringmethods;
+
+public class CharAt {
+
+	public static void main(String[] args) {
+
+		String myFullName = new String("Gowri Sekar");
+		System.out.println(myFullName.charAt(4)); // i
+
+	}
+
+}
