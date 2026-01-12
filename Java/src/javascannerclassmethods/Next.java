ff648a605f942c1e57cf58a91957f1bb03a776e6
Gowri <Gowri@192.168.1.5>
2026-01-12 21:15:55 +0530

    scanned the input from the screen using scanner class next() method

---
+++ Java/src/javascannerclassmethods/Next.java
@@ -0,0 +1,16 @@
+package javascannerclassmethods;
+
+import java.util.Scanner;
+
+public class Next {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String myName = scanner.next();
+		System.out.println("My Name is: " + myName);
+
+	}
+
+}
