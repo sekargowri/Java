4b7b020079d2c02ea6c172d0849eafce91d545cf
Gowri <Gowri@192.168.1.5>
2026-01-12 19:56:01 +0530

    created java string class

---
+++ Java/src/javastring/StringClass.java
@@ -0,0 +1,12 @@
+package javastring;
+
+public class StringClass {
+
+	public static void main(String[] args) {
+
+		String myFullName = new String("Gowri Sekar");
+		System.out.println(myFullName);
+
+	}
+
+}
