c43452a7cc92efd634704af704ba66c20838cc4e
Gowri <Gowri@192.168.1.5>
2026-01-12 19:58:22 +0530

    created java string class object

--- Java/src/javastring/StringClass.java
+++ Java/src/javastring/StringClass.java
@@ -5,7 +5,7 @@ public class StringClass {
 	public static void main(String[] args) {
 
 		String myFullName = new String("Gowri Sekar");
-		System.out.println(myFullName);
+		System.out.println(myFullName); // Gowri Sekar
 
 	}
 
