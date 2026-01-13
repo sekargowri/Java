c4270ba992191720506b0a4a3126bdb918a4028a
Gowri <Gowri@192.168.1.5>
2026-01-13 21:02:26 +0530

    created java modulous operator

---
+++ Java/src/javaarithmeticoperators/ModulousOperator.java
@@ -0,0 +1,14 @@
+package javaarithmeticoperators;
+
+public class ModulousOperator {
+
+	public static void main(String[] args) {
+
+		int number1 = 80;
+		int number2 = 10;
+
+		System.out.println(number1 % number2);
+
+	}
+
+}
