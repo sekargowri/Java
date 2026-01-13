6316c77bf167ea40c5583d5eebb10be0d11910c7
Gowri <Gowri@192.168.1.5>
2026-01-13 20:57:54 +0530

    created java subtraction operator

---
+++ Java/src/javaarithmeticoperators/SubtractionOperator.java
@@ -0,0 +1,14 @@
+package javaarithmeticoperators;
+
+public class SubtractionOperator {
+
+	public static void main(String[] args) {
+
+		int number1 = 70;
+		int number2 = 10;
+
+		System.out.println(number1 - number2);
+
+	}
+
+}
