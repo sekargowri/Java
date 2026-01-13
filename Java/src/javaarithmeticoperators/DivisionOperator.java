739f87b5cb7bdcafa4c8c7d0736571cf3af0d69e
Gowri <Gowri@192.168.1.5>
2026-01-13 21:00:14 +0530

    created java division operator

---
+++ Java/src/javaarithmeticoperators/DivisionOperator.java
@@ -0,0 +1,13 @@
+package javaarithmeticoperators;
+
+public class DivisionOperator {
+
+	public static void main(String[] args) {
+
+		int number1 = 80;
+		int number2 = 10;
+
+		System.out.println(number1 / number2); // return the quotient value
+	}
+
+}
