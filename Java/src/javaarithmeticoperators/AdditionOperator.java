a850a49f404c81b46bb8e58871a1a2c2c11530aa
Gowri <Gowri@192.168.1.5>
2026-01-13 20:56:38 +0530

    created java addition operator

---
+++ Java/src/javaarithmeticoperators/AdditionOperator.java
@@ -0,0 +1,13 @@
+package javaarithmeticoperators;
+
+public class AdditionOperator {
+
+	public static void main(String[] args) {
+
+		int number1 = 10;
+		int number2 = 70;
+
+		System.out.println(number1 + number2);
+	}
+
+}
