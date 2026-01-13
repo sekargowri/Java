35a8cf249c57af9873354e9a0ac3f18be94bb898
Gowri <Gowri@192.168.1.5>
2026-01-13 20:58:58 +0530

    created java multiplication operator

---
+++ Java/src/javaarithmeticoperators/MultiplicationOperator.java
@@ -0,0 +1,14 @@
+package javaarithmeticoperators;
+
+public class MultiplicationOperator {
+
+	public static void main(String[] args) {
+
+		int number1 = 10;
+		int number2 = 70;
+
+		System.out.println(number1 * number2);
+
+	}
+
+}
